51a68bfffd59d65018816a84b09e606f26a6682f
Phantom <bhardwajprashant7045@gmail.com>
2026-04-10 17:09:53 +0530

    iamService added

--- iam-service/src/main/java/com/cts/iam_service/IamServiceApplication.java
+++ iam-service/src/main/java/com/cts/iam_service/IamServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IamServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(IamServiceApplication.class, args);
 	}
