f0ea0e4cec9bd00eb25b4d7ba156a2d1f058f82b
Aakash001 <aakash.a@cognizant.com>
2026-04-10 15:21:40 +0530

    Faculty get mapping is working now

--- iam-service/src/main/java/com/cts/iam_service/application/controller/AppUserController.java
+++ iam-service/src/main/java/com/cts/iam_service/application/controller/AppUserController.java
@@ -2,15 +2,13 @@ package com.cts.iam_service.application.controller;
 
 import com.cts.dto.request.AppUserRegistrationDto;
 import com.cts.dto.request.StudentRegistrationDto;
+import com.cts.dto.response.AppUserDetailByIdDto;
 import com.cts.iam_service.application.service.IAppUserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/appUser")
@@ -24,4 +22,16 @@ public class AppUserController {
         log.info("App user registration request has been initiated successFully by {}",appUserRegistrationDto.getUserName());
         return ResponseEntity.status(200).body(appUserService.appUserRegistration(appUserRegistrationDto));
     }
+    @GetMapping("/findAppUserNameByAppUserId/{appUserId}")
+    public String findAppUserNameByAppUserId(@PathVariable Long appUserId){
+        log.info("Request received to find app user name for app user id: {}", appUserId);
+        return appUserService.findAppUserNameByAppUserId(appUserId);
+    }
+
+    @GetMapping("/findAppUserDetailsByAppUserId/{appUserId}")
+    public AppUserDetailByIdDto findAppUserDetailsByAppUserId(@PathVariable Long appUserId){
+        log.info("Request received to find app user details for app user id: {}", appUserId);
+        return appUserService.findAppUserDetailsByAppUserId(appUserId);
+    }
+
 }
