a0ca9870598a2506efdeb4f4412bd781232016c9
Adii <com>
2026-04-10 14:57:19 +0530

    minor feedback bug fix

--- engagement-service/src/main/java/com/cts/engagement_service/application/util/DtoMapper.java
+++ engagement-service/src/main/java/com/cts/engagement_service/application/util/DtoMapper.java
@@ -8,7 +8,6 @@ public class DtoMapper {
         FeedBack feedBack = new FeedBack();
         feedBack.setMessage(feedbackDto.getComment());
         feedBack.setRating(feedbackDto.getRating());
-        feedBack.setAppUserId(feedbackDto.getAppUserId());
         return feedBack;
     }
 }
