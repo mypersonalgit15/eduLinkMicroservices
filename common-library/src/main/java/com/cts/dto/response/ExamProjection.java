e8832735cb79815d4b62906dfbae26db62759e58
Aakash001 <aakash.a@cognizant.com>
2026-04-10 17:47:58 +0530

    Exam updated

--- common-library/src/main/java/com/cts/dto/response/ExamProjection.java
+++ common-library/src/main/java/com/cts/dto/response/ExamProjection.java
@@ -1,10 +1,13 @@
 package com.cts.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ExamProjection {
     private String examName;
     private LocalDateTime examLocalDateTime;
